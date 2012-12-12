1db36a01adbbfbfbf6404278baade7698f1b9e2a
Antonio Pagano <ing.antoniopagano@gmail.com>
2012-12-12 00:39:56 -0500

    [cleaning] sending war download to background thread

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -Xmx512m -XX:PermSize=64M -XX:MaxPermSize=128M -jar ../lib/jetty-runner.jar --port 4000 --log manager.log --out manager.log ../manager.war   &
+java -Xmx512m -XX:PermSize=64M -XX:MaxPermSize=128M -jar ../lib/jetty-runner.jar --port 4000 --log manager.log --out manager.log manager.war &
 echo "$!" > manager.pid
